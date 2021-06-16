a1dd48f265ee1826c475183164a3090eb72f3ad0
moutga <moutga@gmail.com>
2021-06-16 11:47:11 -0300

    Toma de datos de inputs

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,8 +2,8 @@
 <div>
 	<v-form @submit.prevent="submit" class="w-75 mx-auto " ref="form" >
 		<v-sheet class="pa-2 text-center mb-4" color="white" elevation="1"  >Iniciar sesión</v-sheet>
-		<v-text-field  :rules="reglas" name="usuario" label="Usuario" prepend-inner-icon="mdi-account"></v-text-field>
-		<v-text-field  :rules="reglas" name="contraseña" label="Contraseña" type="password" prepend-inner-icon="mdi-form-textbox-password"></v-text-field>
+		<v-text-field v-model="usuario" :rules="reglas" name="usuario" label="Usuario" prepend-inner-icon="mdi-account"></v-text-field>
+		<v-text-field v-model="contrasena" ref="contrasena" :rules="reglas" name="contraseña" label="Contraseña" type="password" prepend-inner-icon="mdi-form-textbox-password"></v-text-field>
 		<v-btn  class="my-4 blue" type="submit"  >Ingresar</v-btn>
 	</v-form>
 </div>
@@ -14,6 +14,8 @@ export default {
 	name: 'Login',
 	data:function(){
 		return {
+			usuario: '',
+			contrasena: '',
 			reglas: [
 				v => !!v || 'Dato obligatorio'
 			],
@@ -23,7 +25,7 @@ export default {
 	methods: {
 		submit: function submit() {
 			if(this.$refs.form.validate()){
-				this.$refs.form.$el.submit()
+				console.log(this.usuario, this.contrasena);
 			}
 		}
 	},
